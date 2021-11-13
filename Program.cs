22a1c1c865791f621fd5da91ee1065789b2f62b5
ashfana <ashfana@DESKTOP-NPR6MTA>
2021-11-13 16:06:36 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convertion_of_secondToMinutes_viceversa
+{
+    class Program
+    {
+        static (int hours, int minutes, int seconds) Convert(int seconds)
+        {
+            int hours, minutes;
+            seconds = seconds % (24 * 3600);
+            hours = seconds / 3600;
+            seconds %= 3600;
+            minutes = seconds / 60;
+            seconds %= 60;
+
+
+            Console.WriteLine("Hours = " + hours + ", Minutes =" + minutes + ", Seconds = " + seconds);
+            return (hours, minutes, seconds);
+        }
+
+        // convertion_of_secondToMinute
+        public static void Main()
+        {
+            int n = 5000;
+            Convert(n);
+        }
+    }
+}
+
